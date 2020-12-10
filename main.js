b65cd3469a8ae9ccd6d5675256f45308169df77f
Moksh12345 <65060407+Moksh12345@users.noreply.github.com>
2020-12-10 08:52:14 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+//Sentence
+var sentence="My first console project.";
+console.log(sentence);
+//Concat variable
+var first_name="Moksh ";
+var last_name="Yellapu";
+var full_name=first_name.concat(last_name);
+console.log(full_name);
+//Numbers for operations
+var num1=50;
+var num2=25;
+//Addition
+console.log(num1+num2);
+//Subtraction
+console.log(num1-num2);
+//Multiplication
+console.log(num1*num2);
+//Division
+console.log(num1/num2);
